040d4ebc4e9d62bde3a363a51082c4624f490132
Maurice Kollewe T <84001897+maurice-35@users.noreply.github.com>
2022-09-01 23:32:28 +0100

    Add files via upload

---
+++ itv-task/cookies/src/App.js
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+
+const App = () => {
+  // A variable called times that starts at 0
+  // A function called setTimes that updates times
+  const [times, setTimes] = useState(0);
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    let newNumber = times +1;
+    setTimes(newNumber)
+  }
+
+  return (
+    <div className="Cookie">
+      <header className="App-header">
+        <h1>You have clicked {times} cookie{times > 1 ? "s" : ""}</h1>
+        Click to adds
+        <button id="cookie-image" onClick={handleClick}>
+          <img src="https://res.cloudinary.com/doe5zwesw/image/upload/v1661953725/cookie_dp7lsv.png" alt="cookie"
+
+          />
+        </button>
+      </header>
+      <div className="buttons">
+        <div><button>10</button></div>
+        <div><button>20</button></div>
+        <div><button>30</button></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
