e829038d275082ef4f103c795f6f3a0e00cfe39d
mauricekollewe <kollewe.35@gmail.com>
2022-09-02 00:35:14 +0100

    add first remove function

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ const App = () => {
   // A variable called times that starts at 0
   // A function called setTimes that updates times
   const [times, setTimes] = useState(0);
+  const [removeCookies, setRemoveCookies] = useState(0)
 
   const handleClick = (event) => {
     event.preventDefault();
-    let newNumber = times +1;
+    let newNumber = times + 1;
     setTimes(newNumber)
+  }
+
+  const handleClickedCookies = (event) => {
+    event.preventDefault();
+    let cookiesRemoved = times - 10;
+    setRemoveCookies()
+    setTimes(cookiesRemoved)
   }
 
   return (
@@ -18,12 +26,11 @@ const App = () => {
         Click to adds
         <button id="cookie-image" onClick={handleClick}>
           <img src="https://res.cloudinary.com/doe5zwesw/image/upload/v1661953725/cookie_dp7lsv.png" alt="cookie"
-
           />
         </button>
       </header>
       <div className="buttons">
-        <div><button>10</button></div>
+        <div><button onClick={handleClickedCookies} {...removeCookies}>10</button></div>
         <div><button>20</button></div>
         <div><button>30</button></div>
       </div>
