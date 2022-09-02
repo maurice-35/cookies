dd449da4a678f691f3f62d7187e67e8c42301ec2
Maurice Kollewe T <84001897+maurice-35@users.noreply.github.com>
2022-09-02 11:44:05 +0100

    Update App.js

--- itv-task/cookies/src/App.js
+++ itv-task/cookies/src/App.js
@@ -17,55 +17,56 @@ const App = () => {
   const handleClickTen = (event) => {
     event.preventDefault();
     let tenRemovedCookies = times - 10;
-    if(times - 10 < 1) {
+    if (times - 10 < 1) {
       alert('Invalid! Please buy more cookies.')
     } else {
-    setRemoveTenCookies();
-    setTimes(tenRemovedCookies);
-  };
-}
+      setRemoveTenCookies();
+      setTimes(tenRemovedCookies);
+    };
+  }
 
   const handleClickTwenty = (event) => {
     event.preventDefault();
     let twentyRemovedCookies = times - 20;
-    if(times - 20 < 1) {
+    if (times - 20 < 1) {
       alert('Invalid! Please buy more cookies.')
     } else {
-    setRemoveTwentyCookies();
-    setTimes(twentyRemovedCookies);
-  };
-}
+      setRemoveTwentyCookies();
+      setTimes(twentyRemovedCookies);
+    };
+  }
 
   const handleClickThirty = (event) => {
     event.preventDefault();
     let thirtyRemovedCookies = times - 30;
-    if(times - 30 < 1) {
+    if (times - 30 < 1) {
       alert('Invalid! Please buy more cookies.')
     } else {
-    setRemoveThirtyCookies();
-    setTimes(thirtyRemovedCookies);
-  };
-}
+      setRemoveThirtyCookies();
+      setTimes(thirtyRemovedCookies);
+    };
+  }
 
   return (
-    <div className="Cookie">
-      <header className="App-header">
-        <h1>You have clicked {times} cookie{times > 1 ? "s" : ""}</h1>
-        Click to add cookies
-        <button id="cookie-image" onClick={handleClick}>
-          <img src="https://res.cloudinary.com/doe5zwesw/image/upload/v1661953725/cookie_dp7lsv.png" alt="cookie"
-          />
-        </button>
-      </header>
-      <div className="buttons">
-        <div><button onClick={handleClickTen} {...removeTenCookies}>10</button></div>
-        <div><button onClick={handleClickTwenty} {...removeTwentyCookies}>20</button></div>
-        <div><button onClick={handleClickThirty} {...removeThirtyCookies}>30</button></div>
+    <section>
+      <div className="Cookie">
+        <header className="App-header">
+          <h1>You have clicked {times} cookie{times > 1 ? "s" : ""}</h1>
+          Click to add cookies
+          <button id="cookie-image" onClick={handleClick}>
+            <img src="https://res.cloudinary.com/doe5zwesw/image/upload/v1661953725/cookie_dp7lsv.png" alt="cookie"
+            />
+          </button>
+        </header>
+        <div className="buttons">
+          <div><button onClick={handleClickTen} {...removeTenCookies}>10</button></div>
+          <div><button onClick={handleClickTwenty} {...removeTwentyCookies}>20</button></div>
+          <div><button onClick={handleClickThirty} {...removeThirtyCookies}>30</button></div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
 
 export default App;
-
 
