52468240304061f7717aaf5f735c1ac4bf72c97b
Maurice Kollewe T <84001897+maurice-35@users.noreply.github.com>
2022-09-12 22:37:52 +0100

    Create getHands function, disable buttons & set popups
    Create getHands function, disable buttons & set popups

--- itv-task/cookies/src/App.js
+++ itv-task/cookies/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 const App = () => {
   // A variable called times that starts at 0
@@ -6,36 +6,72 @@ const App = () => {
   const [cookies, setCookies] = useState(0);
   const [helpers, setHelpers] = useState(0);
 
-
   const handleBuy = (price) => {
     if (cookies - price < 0) {
-      alert('Invalid! Please buy more cookies.')
     } else {
       console.log("you bought a help");
       setHelpers(helpers + price);
       setCookies(cookies - price);
     }
-  }
+  };
 
+  const getHands = () => {
+    let string = "";
+    for (let i = 0; i < helpers; i++) {
+      string = string + "🖐";
+    }
+    return string;
+  };
+
+  useEffect(() => {
+    const add = setInterval(() => {
+      setCookies(cookies + helpers);
+    }, 1000);
+    return () => clearInterval(add);
+  }, [cookies, helpers]);
 
   return (
     <section className="containers">
       <section className="container1">
         <div className="Cookie">
           <header className="App-header">
-            <h1>You have clicked {cookies} cookie{cookies > 1 ? "s" : ""}</h1>
-            <p className="helpers">You have<span><strong>{helpers}</strong></span>helpers</p>
+            <h1>
+              You have clicked {cookies} cookie{cookies > 1 ? "s" : ""}
+            </h1>
+            <p>Click to add cookies</p>
+            <p>{getHands()}</p>
+            <p className="helpers">
+              You have
+              <span>
+                <strong>{helpers}</strong>
+              </span>
+              helpers
+            </p>
             <div className="image">
               <button id="cookie-image" onClick={() => setCookies(cookies + 1)}>
-                <img src="https://thumbs.gfycat.com/BiodegradableBlueClumber.webp" alt="cookie"
+                <img
+                  src="https://thumbs.gfycat.com/BiodegradableBlueClumber.webp"
+                  alt="cookie"
                 />
               </button>
             </div>
           </header>
           <div className="buttons">
-            <div><button onClick={() => handleBuy(10)}>10</button></div>
-            <div><button onClick={() => handleBuy(20)}>20</button></div>
-            <div><button onClick={() => handleBuy(30)}>30</button></div>
+            <div>
+              <button disabled={cookies < 1} onClick={() => handleBuy(1)}>
+                1
+              </button>
+            </div>
+            <div>
+              <button disabled={cookies < 2} onClick={() => handleBuy(2)}>
+                2
+              </button>
+            </div>
+            <div>
+              <button disabled={cookies < 3} onClick={() => handleBuy(3)}>
+                3
+              </button>
+            </div>
           </div>
         </div>
       </section>
@@ -43,9 +79,20 @@ const App = () => {
         <div className="Cookie">
           <header className="App-header">
             <div className="app-section2">
-              <img src="https://images.pexels.com/photos/531880/pexels-photo-531880.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="background"
+              <img
+                src="https://images.pexels.com/photos/531880/pexels-photo-531880.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt="background"
               />
             </div>
+            <span>
+              {helpers >= 100 && <strong>👍🏾 Well Done! Keep On!</strong>}
+            </span>
+            <span>
+              {helpers >= 200 && <strong>🤝 Brave Shark! Keep On!</strong>}
+            </span>
+            <span>
+              {helpers >= 300 && <strong>🦾 Curious Tycoon! Keep On!</strong>}
+            </span>
           </header>
         </div>
       </section>
@@ -53,7 +100,9 @@ const App = () => {
         <div className="Cookie">
           <header className="App-header">
             <div className="app-section3">
-              <img src="https://images.pexels.com/photos/531880/pexels-photo-531880.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="background"
+              <img
+                src="https://images.pexels.com/photos/531880/pexels-photo-531880.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt="background"
               />
             </div>
           </header>
@@ -61,7 +110,6 @@ const App = () => {
       </section>
     </section>
   );
-}
+};
 
 export default App;
-
