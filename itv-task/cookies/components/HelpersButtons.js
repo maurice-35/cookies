8d2c179a9ef014872515066ec0ac0fba0eb01513
Maurice Kollewe T <84001897+maurice-35@users.noreply.github.com>
2022-09-12 23:10:26 +0100

    Delete HelpersButtons.js

--- itv-task/cookies/components/HelpersButtons.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const HelpersButtons = ({ cookies, handleClick, price }) => {
-	return (
-		<div><button onClick={() => handleClick(price)} disabled={cookies < price}>{price}</button></div>
-	)
-}
-
-export default HelpersButtons;
