87025c3db780a61737ac24ee66ff80bc829e0eb2
Maurice Kollewe T <84001897+maurice-35@users.noreply.github.com>
2022-09-13 10:19:30 +0100

    Delete itv-task/cookies/src directory

--- itv-task/cookies/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders instruction', () => {
-  render(<App />);
-  const header = screen.getByText(/Click to add cookies/i);
-  expect(header).toBeInTheDocument();
-});
