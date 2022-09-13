5cbdc26fc05bcb9a2fe3ed9670ceaf4b38f8997a
Maurice Kollewe T <84001897+maurice-35@users.noreply.github.com>
2022-09-13 09:36:26 +0100

    Added a test

--- itv-task/cookies/src/App.test.js
+++ itv-task/cookies/src/App.test.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders instruction', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const header = screen.getByText(/Click to add cookies/i);
+  expect(header).toBeInTheDocument();
 });
