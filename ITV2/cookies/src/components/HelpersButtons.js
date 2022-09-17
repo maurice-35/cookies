ebd37df344575ecbcfba2df395e67e67cdfac483
Maurice Kollewe T <84001897+maurice-35@users.noreply.github.com>
2022-09-17 14:24:47 +0100

    Add button Id

--- ITV2/cookies/src/components/HelpersButtons.js
+++ ITV2/cookies/src/components/HelpersButtons.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const HelpersButtons = ({ cookies, handleClick, price }) => {
-	return (
-		<div><button onClick={() => handleClick(price)} disabled={cookies < price}>{price}</button></div>
-	)
-}
+  return (
+    <div>
+      <button
+        id="butt"
+        onClick={() => handleClick(price)}
+        disabled={cookies < price}
+      >
+        {price}
+      </button>
+    </div>
+  );
+};
 
 export default HelpersButtons;
